9303817080deb8206e638a5501083cb20ff8a735
Daniel <danielagustianyoali@gmail.com>
2022-07-18 09:02:35 +0700

    commit favicon

--- resources/views/master2.blade.php
+++ resources/views/master2.blade.php
@@ -10,7 +10,10 @@
     <meta name="robots" content="index, follow" />
     <meta name="author" content="Mediakagumi.com" />
     <meta name="google-site-verification" content="oQk4soQuvFJqEjTkl6S6xFgrd-MYazzr1S_eapX8ikE" />
-
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
     <!-- Stylesheets
  ============================================= -->
     <link
